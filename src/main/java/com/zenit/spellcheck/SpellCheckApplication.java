12353a6c587d29778caf2ee7fa6395e1e550c7bc
zenitaxians <neziri.zenit@vinci-energies.net>
2024-01-13 16:29:54 +0100

    Created spell-check project and connected to psql database

---
+++ src/main/java/com/zenit/spellcheck/SpellCheckApplication.java
@@ -0,0 +1,15 @@
+package com.zenit.spellcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellCheckApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpellCheckApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/zenit/spellcheck/SpellCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenit.spellcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpellCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
