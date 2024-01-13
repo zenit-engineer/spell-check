e9a22323487e629fe36e68742af2e8e7450468a1
zenitaxians <neziri.zenit@vinci-energies.net>
2024-01-13 17:00:23 +0100

    Created Controller For SpellCheck

---
+++ src/main/java/com/zenit/spellcheck/controller/SpellCheckerController.java
@@ -0,0 +1,40 @@
+package com.zenit.spellcheck.controller;
+
+
+import com.zenit.spellcheck.service.SpellCheckService;
+import com.zenit.spellcheck.util.ResponseJsonBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "api/v1/spell-check")
+public class SpellCheckerController {
+
+    private final SpellCheckService spellCheckService;
+
+    @GetMapping("/test")
+    public String test(){
+        return "Test";
+    }
+
+    @GetMapping("/match")
+    public ResponseEntity<ResponseJsonBody> spellCheck(@RequestParam String word){
+
+        ResponseJsonBody updateProjectByIdResponse = spellCheckService.spellCheck(word);
+
+        return new ResponseEntity<>(
+
+                updateProjectByIdResponse,
+                updateProjectByIdResponse.getStatus()
+
+        );
+
+    }
+
+}
+
