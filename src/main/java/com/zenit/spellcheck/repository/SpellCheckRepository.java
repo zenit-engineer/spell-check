c7c2b17f8efbcf430b91c8b078e5c71665aa71f6
zenitaxians <neziri.zenit@vinci-energies.net>
2024-01-14 11:59:48 +0100

    Optimised spell check logic from code to the query level

--- src/main/java/com/zenit/spellcheck/repository/SpellCheckRepository.java
+++ src/main/java/com/zenit/spellcheck/repository/SpellCheckRepository.java
@@ -2,10 +2,22 @@ package com.zenit.spellcheck.repository;
 
 import com.zenit.spellcheck.model.AlbanianDictionary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.math.BigInteger;
+import java.util.List;
 
 @Repository
 public interface SpellCheckRepository extends JpaRepository<AlbanianDictionary, BigInteger > {
+
+    @Query(value = """
+            SELECT a.word
+            FROM albanian_dictionary a
+            WHERE similarity(a.word, :word) > 0.1
+            ORDER BY similarity(a.word, :word) DESC
+            LIMIT 3
+            """, nativeQuery = true)
+    List<String> findWordBySimilarityScore(@Param("word") String word);
+
 }
